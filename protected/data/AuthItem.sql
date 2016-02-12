a9471609f554c1e739ed461a5f0f42100f059b47
redx88 <red_x88@yahoo.com>
2016-02-12 09:54:54 +0800

    Update Data Files
    Update for protected/data

--- protected/data/AuthItem.sql
+++ protected/data/AuthItem.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 21, 2015 at 11:56 PM
+-- Generation Time: Feb 12, 2016 at 09:33 AM
 -- Server version: 5.6.21
 -- PHP Version: 5.6.3
 
